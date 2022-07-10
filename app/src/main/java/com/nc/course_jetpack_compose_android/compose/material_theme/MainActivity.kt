2a0b59bdff03a123c6efac9210df877e13f5fa86
Andres Giraldo <andresganc@gmail.com>
2022-07-10 15:15:01 -0500

    New: Material Theme

--- app/src/main/java/com/nc/course_jetpack_compose_android/compose/material_theme/MainActivity.kt
+++ app/src/main/java/com/nc/course_jetpack_compose_android/compose/material_theme/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nc.course_jetpack_compose_android.compose.row
+package com.nc.course_jetpack_compose_android.compose.material_theme
 
 import android.content.res.Configuration
 import android.os.Bundle
@@ -19,7 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nc.course_jetpack_compose_android.R
-import com.nc.course_jetpack_compose_android.compose.row.ui.theme.CoursejetpackcomposeandroidTheme
+import com.nc.course_jetpack_compose_android.compose.material_theme.ui.theme.CoursejetpackcomposeandroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +44,8 @@ fun Card01() {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = "Image profile",
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier
+                .padding(10.dp)
                 .size(80.dp)
                 .clip(CircleShape)
                 .background(Color.Cyan)
@@ -55,12 +56,32 @@ fun Card01() {
                 color = MaterialTheme.colors.primary,
                 style = MaterialTheme.typography.h6
             )
-
+            Text(
+                text = "Name: Marco",
+                style = MaterialTheme.typography.body2
+            )
+            Text(
+                text = "Lastname: Giraldo Gomez",
+                style = MaterialTheme.typography.body2
+            )
+            Text(
+                text = "Age: 5",
+                style = MaterialTheme.typography.body2
+            )
+            Text(
+                text = "Date of Birth: 20 December 2017",
+                style = MaterialTheme.typography.body2
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     CoursejetpackcomposeandroidTheme {
